38a0218f81a69f35378e6eac10a9e3ef5a60ed72
misterkwao <vigilantkwao@gmail.com>
2024-01-26 15:02:33 +0000

    path

--- Back-End/attachmentUpload.js
+++ Back-End/attachmentUpload.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req,file,callback) {
-      const folderName = `$/userAttachments/${req.user.userID}`;
+      const folderName = `/userAttachments/${req.user.userID}`;
 
       //checking if userfolder already exists
       try {
