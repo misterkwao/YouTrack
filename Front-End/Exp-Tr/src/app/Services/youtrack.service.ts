5a81628988221d3bd7435f3876bc435ce6b79353
misterkwao <vigilantkwao@gmail.com>
2024-03-05 16:40:40 +0000

    cyclic

--- Front-End/Exp-Tr/src/app/Services/youtrack.service.ts
+++ Front-End/Exp-Tr/src/app/Services/youtrack.service.ts
@@ -20,7 +20,7 @@ export class YoutrackService {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${sessionStorage.getItem("token")}`);
     switch(levelType){
        case "Level 1":
-         return this.http.post("https://youtrack-hfl0.onrender.com/api/v1/admin/manageuser",
+         return this.http.post("https://youtrack.cyclic.app/api/v1/admin/manageuser",
          {
            name: username,
            email: email,
@@ -31,7 +31,7 @@ export class YoutrackService {
          {'headers': headers}
          )
          default:
-          return this.http.post("https://youtrack-hfl0.onrender.com/api/v1/admin/manageuser",
+          return this.http.post("https://youtrack.cyclic.app/api/v1/admin/manageuser",
           {
             name: username,
             email: email,
@@ -48,7 +48,7 @@ export class YoutrackService {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${sessionStorage.getItem("token")}`);
     switch(levelType){
        case "Level 1":
-         return this.http.patch(`https://youtrack-hfl0.onrender.com/api/v1/admin/manageuser/${profileID}`,
+         return this.http.patch(`https://youtrack.cyclic.app/api/v1/admin/manageuser/${profileID}`,
          {
            role: "level 1",
            permissions :["create","update","delete"]
@@ -56,7 +56,7 @@ export class YoutrackService {
          {'headers': headers}
          )
          case "Level 2":
-          return this.http.patch(`https://youtrack-hfl0.onrender.com/api/v1/admin/manageuser/${profileID}`,
+          return this.http.patch(`https://youtrack.cyclic.app/api/v1/admin/manageuser/${profileID}`,
           {
             role:"level 2",
             permissions:["create","update","delete","eur"]
@@ -64,7 +64,7 @@ export class YoutrackService {
           {'headers': headers}
           )
         default:
-          return this.http.patch(`https://youtrack-hfl0.onrender.com/api/v1/admin/manageuser/${profileID}`,
+          return this.http.patch(`https://youtrack.cyclic.app/api/v1/admin/manageuser/${profileID}`,
           {
             role:"level 2",
             permissions:["create","update","delete"]
@@ -75,7 +75,7 @@ export class YoutrackService {
   }
 
   logIn(email: string, password: string): Observable<logInModel>{
-    return this.http.post<logInModel>("https://youtrack-hfl0.onrender.com/api/v1/auth/login",
+    return this.http.post<logInModel>("https://youtrack.cyclic.app/api/v1/auth/login",
     {
       email: email,
       password: password
@@ -84,7 +84,7 @@ export class YoutrackService {
 }
 
 logInAdmin(email: string, password: string): Observable<logInModel>{
-  return this.http.post<logInModel>("https://youtrack-hfl0.onrender.com/api/v1/adminAuth/login",
+  return this.http.post<logInModel>("https://youtrack.cyclic.app/api/v1/adminAuth/login",
   {
     email: email,
     password: password
@@ -95,12 +95,12 @@ logInAdmin(email: string, password: string): Observable<logInModel>{
 
   getUserProfile(): Observable<userModel>{
     const headers = new HttpHeaders().set('Authorization', `Bearer ${sessionStorage.getItem("token")}`);
-    return this.http.get<userModel>("https://youtrack-hfl0.onrender.com/api/v1/user",{'headers': headers});
+    return this.http.get<userModel>("https://youtrack.cyclic.app/api/v1/user",{'headers': headers});
   };
 
   getAdminProfile(): Observable<adminModel>{
     const headers = new HttpHeaders().set('Authorization', `Bearer ${sessionStorage.getItem("token")}`);
-    return this.http.get<adminModel>("https://youtrack-hfl0.onrender.com/api/v1/admin",{'headers': headers});
+    return this.http.get<adminModel>("https://youtrack.cyclic.app/api/v1/admin",{'headers': headers});
   }
    
   updateUserProfile(updateType: string,updateValue: any){
@@ -109,23 +109,23 @@ logInAdmin(email: string, password: string): Observable<logInModel>{
       case 'profilePic':
         const formData = new FormData();
         formData.append('profilePictureURL', updateValue)//field and value
-       return this.http.patch("https://youtrack-hfl0.onrender.com/api/v1/user",formData,{'headers': headers});
+       return this.http.patch("https://youtrack.cyclic.app/api/v1/user",formData,{'headers': headers});
       case 'username':
-       return this.http.patch("https://youtrack-hfl0.onrender.com/api/v1/user",{user:updateValue},{'headers': headers});
+       return this.http.patch("https://youtrack.cyclic.app/api/v1/user",{user:updateValue},{'headers': headers});
      case 'category':
-      return this.http.patch("https://youtrack-hfl0.onrender.com/api/v1/user",{userCategory:updateValue},{'headers': headers});
+      return this.http.patch("https://youtrack.cyclic.app/api/v1/user",{userCategory:updateValue},{'headers': headers});
      case 'filterW':
-      return this.http.patch("https://youtrack-hfl0.onrender.com/api/v1/user",{filterBy:updateValue},{'headers': headers});
+      return this.http.patch("https://youtrack.cyclic.app/api/v1/user",{filterBy:updateValue},{'headers': headers});
      case 'filterY':
-      return this.http.patch("https://youtrack-hfl0.onrender.com/api/v1/user",{filterBy:updateValue},{'headers': headers});
+      return this.http.patch("https://youtrack.cyclic.app/api/v1/user",{filterBy:updateValue},{'headers': headers});
      default:
-      return this.http.patch("https://youtrack-hfl0.onrender.com/api/v1/user",{currencyType:updateValue},{'headers': headers})
+      return this.http.patch("https://youtrack.cyclic.app/api/v1/user",{currencyType:updateValue},{'headers': headers})
   }
   }
 
   deleteUser(id:string){
     const headers = new HttpHeaders().set('Authorization', `Bearer ${sessionStorage.getItem("token")}`);
-   return this.http.delete(`https://youtrack-hfl0.onrender.com/api/v1/admin/manageuser/${id}`,{'headers': headers});
+   return this.http.delete(`https://youtrack.cyclic.app/api/v1/admin/manageuser/${id}`,{'headers': headers});
   }
   
   addorupdate(description: string,category:any,attachmentUrl:any,pv:string,amount: any,record: string,addorupdate:boolean,recordID:any){
@@ -139,7 +139,7 @@ logInAdmin(email: string, password: string): Observable<logInModel>{
       formData.append('pv',pv)
       formData.append('category',category);
       formData.append('description',description);
-      return this.http.post("https://youtrack-hfl0.onrender.com/api/v1/user/data/records",formData,{'headers':headers});
+      return this.http.post("https://youtrack.cyclic.app/api/v1/user/data/records",formData,{'headers':headers});
 
     }
     else{
@@ -163,7 +163,7 @@ logInAdmin(email: string, password: string): Observable<logInModel>{
         formData.append('description',description);
       }
       
-      return this.http.patch(`https://youtrack-hfl0.onrender.com/api/v1/user/data/records/${recordID}`,formData,{'headers':headers});
+      return this.http.patch(`https://youtrack.cyclic.app/api/v1/user/data/records/${recordID}`,formData,{'headers':headers});
       
     }
   }
@@ -191,49 +191,49 @@ logInAdmin(email: string, password: string): Observable<logInModel>{
       if(description){
         formData.append('description',description);
       }
-      return this.http.patch(`https://youtrack-hfl0.onrender.com/api/v1/user/manageUsers/records/${recordID}`,formData,{'headers':headers});
+      return this.http.patch(`https://youtrack.cyclic.app/api/v1/user/manageUsers/records/${recordID}`,formData,{'headers':headers});
   
 
   }
 
   deleteRecord(id: string){
     const headers = new HttpHeaders().set('Authorization', `Bearer ${sessionStorage.getItem("token")}`);
-       return this.http.delete(`https://youtrack-hfl0.onrender.com/api/v1/user/data/records/${id}`,{'headers':headers})
+       return this.http.delete(`https://youtrack.cyclic.app/api/v1/user/data/records/${id}`,{'headers':headers})
   }
 
   getYearData(): Observable<yearModel>{
     const headers = new HttpHeaders().set('Authorization', `Bearer ${sessionStorage.getItem("token")}`);
-    return this.http.get<yearModel>("https://youtrack-hfl0.onrender.com/api/v1/user/data?filterBy=year",{'headers':headers});
+    return this.http.get<yearModel>("https://youtrack.cyclic.app/api/v1/user/data?filterBy=year",{'headers':headers});
   }
 
   getWeekData(): Observable<weekModel>{
     const headers = new HttpHeaders().set('Authorization', `Bearer ${sessionStorage.getItem("token")}`);
-    return this.http.get<weekModel>("https://youtrack-hfl0.onrender.com/api/v1/user/data?filterBy=week",{'headers':headers});
+    return this.http.get<weekModel>("https://youtrack.cyclic.app/api/v1/user/data?filterBy=week",{'headers':headers});
   }
 
   getManageUsersStats(): Observable<muModel>{
     const headers = new HttpHeaders().set('Authorization', `Bearer ${sessionStorage.getItem("token")}`);
-    return this.http.get<muModel>("https://youtrack-hfl0.onrender.com/api/v1/user/manageUsers",{'headers':headers});
+    return this.http.get<muModel>("https://youtrack.cyclic.app/api/v1/user/manageUsers",{'headers':headers});
   }
 
   getAllUserTransactions(): Observable<AllUserTransactions>{
     const headers = new HttpHeaders().set('Authorization', `Bearer ${sessionStorage.getItem("token")}`);
-    return this.http.get<AllUserTransactions>("https://youtrack-hfl0.onrender.com/api/v1/user/manageUsers/records",{'headers':headers});
+    return this.http.get<AllUserTransactions>("https://youtrack.cyclic.app/api/v1/user/manageUsers/records",{'headers':headers});
   }
 
   getAllUsers(): Observable<allUsers>{
     const headers = new HttpHeaders().set('Authorization', `Bearer ${sessionStorage.getItem("token")}`);
-    return this.http.get<allUsers>("https://youtrack-hfl0.onrender.com/api/v1/admin/manageuser",{'headers':headers});
+    return this.http.get<allUsers>("https://youtrack.cyclic.app/api/v1/admin/manageuser",{'headers':headers});
   }
 
   sendLink(email:string): Observable<forgotPasswordModel>{
-    return this.http.post<forgotPasswordModel>("https://youtrack-hfl0.onrender.com/api/v1/auth/forgotPassword", {
+    return this.http.post<forgotPasswordModel>("https://youtrack.cyclic.app/api/v1/auth/forgotPassword", {
       email: email
     })
   }
 
   changePassword(token:string, password:string): Observable<forgotPasswordModel>{
-    return this.http.patch<forgotPasswordModel>(`https://youtrack-hfl0.onrender.com/api/v1/auth/resetPassword/${token}`, {
+    return this.http.patch<forgotPasswordModel>(`https://youtrack.cyclic.app/api/v1/auth/resetPassword/${token}`, {
       password: password
     })
   }
